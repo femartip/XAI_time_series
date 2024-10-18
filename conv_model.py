0919ddd083069475b900a4390b19195ca2ab84d7
femartip <felix.marti99@gmail.com>
2024-10-18 13:43:29 +0200

    Created black-box model insiped in ODS algorithm model

---
+++ conv_model.py
@@ -0,0 +1,39 @@
+import torch
+
+# Classifier for time series classification
+class ConvClassifier(torch.nn.Module):
+
+    def __init__(self, input_size, num_classes):
+        super(ConvClassifier, self).__init__()
+
+
+        self.conv1 = torch.nn.Conv1d(in_channels=1, out_channels=128, kernel_size=8)
+        self.batch_norm1 = torch.nn.BatchNorm1d(128)
+        
+        self.conv2 = torch.nn.Conv1d(in_channels=128, out_channels=256, kernel_size=5)
+        self.batch_norm2 = torch.nn.BatchNorm1d(256)
+
+        self.conv3 = torch.nn.Conv1d(in_channels=256, out_channels=128, kernel_size=3)
+        self.batch_norm3 = torch.nn.BatchNorm1d(128)
+
+        self.global_pool = torch.nn.AdaptiveAvgPool1d(1)
+
+        self.fc = torch.nn.Linear(128, 1)
+
+        self.sigmoid = torch.nn.Sigmoid()
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+        x = self.relu(self.batch_norm2(self.conv2(x)))
+        x = self.relu(self.batch_norm3(self.conv3(x)))
+        x = self.global_pool(x)
+        x = torch.flatten(x, 1)
+        x = self.sigmoid(self.fc(x))
+
+        return x
+    
+
+
+        
+        
---
+++ train_conv_model.py
@@ -0,0 +1,120 @@
+import torch
+from torcheval.metrics import BinaryAccuracy
+import argparse
+from data import load_data
+import conv_model
+import numpy as np
+import os
+import logging
+import matplotlib.pyplot as plt
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def plot_metrics(train_metrics, train_losses, val_metrics, val_losses):
+    fig, ax = plt.subplots(2, 1, figsize=(10, 10))
+    ax[0].plot(train_metrics, label='Train')
+    ax[0].plot(val_metrics, label='Validation')
+    ax[0].set_title('Accuracy')
+    ax[0].set_xlabel('Epoch')
+    ax[0].set_ylabel('Accuracy')
+    ax[0].legend()
+
+    ax[1].plot(train_losses, label='Train')
+    ax[1].plot(val_losses, label='Validation')
+    ax[1].set_title('Loss')
+    ax[1].set_xlabel('Epoch')
+    ax[1].set_ylabel('Loss')
+    ax[1].legend()
+
+    plt.show()
+
+def train_conv_model(X,y, X_val, y_val):
+    learning_rate = 0.01
+    epochs = 100
+    batch_size = 1024
+
+    train_metrics = []
+    train_losses = []
+    val_metrics = []
+    val_losses = []
+
+    X_val = torch.tensor(X_val, dtype=torch.float32).to(DEVICE)
+    X_val = X_val.unsqueeze(1)
+    y_val = torch.tensor(y_val, dtype=torch.float32).to(DEVICE)
+    input_size = X.shape[1]
+    logging.info("Input size: " + str(input_size))
+    num_classes = len(set(y))
+    logging.info("Number of classes: " + str(num_classes))
+    logging.debug(f"y: {y}")
+
+    model = conv_model.ConvClassifier(input_size, num_classes)
+    model.train()
+    model.to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = torch.nn.BCELoss()
+    metric = BinaryAccuracy()
+    metric_val = BinaryAccuracy()
+    #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5, verbose=True)
+
+    for epoch in range(epochs):
+        for i in range(0, len(X), batch_size):
+            X_batch = torch.tensor(X[i:i+batch_size], dtype=torch.float32).to(DEVICE)
+            X_batch = X_batch.unsqueeze(1)
+            logging.debug("X_batch shape: " + str(X_batch.shape))
+            logging.debug(f"X_batch: {X_batch}")
+            X_batch = X_batch.to(DEVICE)
+            y_batch = torch.tensor(y[i:i+batch_size], dtype=torch.float32).to(DEVICE)
+            #y_batch = y_batch.unsqueeze(1)
+            logging.debug("y_batch shape: " + str(y_batch.shape))
+
+            optimizer.zero_grad()
+            output = model(X_batch)
+            output = torch.squeeze(output, 1)
+            logging.debug("Output shape: " + str(output.shape))
+            loss = criterion(output, y_batch)
+            loss.backward()
+            optimizer.step()
+            #scheduler.step(loss)
+            metric.update(output, y_batch)
+
+            if i % 1 == 0:
+                train_losses.append(loss.item())
+                train_metrics.append(metric.compute())
+                print(f'Epoch {epoch}, Loss: {loss.item()}, Accuracy: {metric.compute()}')
+                with torch.no_grad():
+                    logging.debug("Validation shape: " + str(X_val.shape))
+                    val_output = model(torch.tensor(X_val, dtype=torch.float32).to(DEVICE))
+                    val_output = torch.squeeze(val_output, 1)
+                    val_loss = criterion(val_output, torch.tensor(y_val, dtype=torch.float32))
+                    metric_val.update(val_output, y_val)
+                    val_losses.append(val_loss.item())
+                    val_metrics.append(metric_val.compute())
+                    print(f'Validation Loss: {val_loss.item()}, Accuracy: {metric_val.compute()}')
+
+    print(f'Final Accuracy: {metric.compute()}')
+    print(f'Final Validation Accuracy: {metric_val.compute()}')
+
+    plot_metrics(train_metrics, train_losses, val_metrics, val_losses)
+
+    return model    
+    
+def save_model(model, model_path='model.pth'):
+    torch.save(model.state_dict(), model_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset_name', type=str, help='Dataset to use, supported: Chinatown, ECG200, ItalyPowerDemand')
+    parser.add_argument('model_file_name', type=str, help='Path to save the model')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    X_train = load_data.load_dataset(args.dataset_name)
+    y_train = load_data.load_dataset_labels(args.dataset_name)
+    X_val = load_data.load_dataset(args.dataset_name, data_type='VALIDATION')
+    y_val = load_data.load_dataset_labels(args.dataset_name, data_type='VALIDATION')
+
+    model = train_conv_model(X_train, y_train, X_val, y_val)
+
+    save_model(model, os.path.join('models', args.model_file_name))
