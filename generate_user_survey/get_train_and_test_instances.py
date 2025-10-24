0e35428e78cdd4e2101774c0bc5356e83f6e76a8
BrigtHaavardstun <Brigt_Arve@outlook.com>
2025-10-24 17:16:23 +0200

    [survey] code formatting🧹

--- generate_user_survey/get_train_and_test_instances.py
+++ generate_user_survey/get_train_and_test_instances.py
@@ -49,27 +49,28 @@ def need_to_train_and_test(dataset):
     return not have_all
 
 
-
-
-def get_train_and_test_index(dataset, remake=False)->Tuple[Dict[str,np.ndarray], np.ndarray]:
+def get_train_and_test_index(dataset, remake=False) -> Tuple[Dict[str, np.ndarray], np.ndarray]:
     if remake or need_to_train_and_test(dataset):
+        warnings.warn("Re-selecting prototype and test instances")
         make_and_save_train_test_instance(dataset)
 
     train_idx = {}
-    for c in [0,1]:
+    for c in [0, 1]:
         train_idx[c] = np.load(f"generate_user_survey/prototype_and_test/{dataset}_train_label_idx_{c}.npy")
     test_idx = np.load(f"generate_user_survey/prototype_and_test/{dataset}_test_idx.npy")
     return train_idx, test_idx
 
-def simplify_instance_to_loyalty_level(instances: List[float], dataset:str,loyalty_level:str,verbose:bool=False) -> np.ndarray:
+
+def simplify_instance_to_loyalty_level(instances: List[float], dataset: str, loyalty_level: str,
+                                       verbose: bool = False) -> np.ndarray:
     alpha, num_segments = loyalty_value_for_each_dataset(dataset)[loyalty_level]
 
-    simplified_segTS = get_RDP_simplification(np.array(instances),epsilon=alpha)
+    simplified_segTS = get_RDP_simplification(np.array(instances), epsilon=alpha)
     simplified_instances = np.array([segTS.line_version for segTS in simplified_segTS])
     return simplified_instances
 
 
-def get_train_and_test_instances(dataset, loyalty_level)->Tuple[Dict[str,np.ndarray], np.ndarray]:
+def get_train_and_test_instances(dataset, loyalty_level) -> Tuple[Dict[str, np.ndarray], np.ndarray]:
     train_ts = load_dataset(dataset, data_type=f"TRAIN{dataset_extra}")
     test_ts = load_dataset(dataset, data_type=f"TEST{dataset_extra}")
     train_instances, test_instances = get_train_and_test_index(dataset)
@@ -77,8 +78,8 @@ def get_train_and_test_instances(dataset, loyalty_level)->Tuple[Dict[str,np.ndar
     for c in train_instances.keys():
         full_train_instances[c] = train_ts[train_instances[c]]
 
-    full_test_instances =  test_ts[test_instances]
-    full_train_instances_simplified= {
+    full_test_instances = test_ts[test_instances]
+    full_train_instances_simplified = {
         0: simplify_instance_to_loyalty_level(full_train_instances[0], dataset, loyalty_level),
         1: simplify_instance_to_loyalty_level(full_train_instances[1], dataset, loyalty_level)}
     full_test_instances_simplified = simplify_instance_to_loyalty_level(full_test_instances, dataset, loyalty_level)
--- make_full_survey.py
+++ make_full_survey.py
@@ -1,17 +1,15 @@
+import io
 from typing import Dict, List, Tuple
 
-import PIL
 import numpy as np
-import io
-
 from matplotlib import pyplot as plt
-from reportlab.platypus import (Frame, FrameBreak,PageTemplate,BaseDocTemplate,PageBreak,Paragraph,Image, Spacer)
-from reportlab.platypus.flowables import Flowable
-from reportlab.lib.units import inch
-from reportlab.lib.pagesizes import letter
 from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import Frame, FrameBreak,PageTemplate,BaseDocTemplate,PageBreak,Paragraph, Table, TableStyle
+from reportlab.lib.units import inch
+from reportlab.platypus import Frame, FrameBreak, PageTemplate, BaseDocTemplate, PageBreak, Paragraph, Table, TableStyle
+from reportlab.platypus import (Image, Spacer)
+from reportlab.platypus.flowables import Flowable
 
 from generate_user_survey.configurations import get_dataset_and_loyalty_from_config, get_config_of_group
 from generate_user_survey.get_train_and_test_instances import get_train_and_test_instances
@@ -20,6 +18,7 @@ styles = getSampleStyleSheet()
 global BODY_TEXT_STYLE
 BODY_TEXT_STYLE = styles["BodyText"]
 BODY_TEXT_STYLE.fontSize = 10
+
 
 class ClassSwatch(Flowable):
     def __init__(self, class_name: str, fill_color: colors.Color, box=12, gap=4):
@@ -39,21 +38,24 @@ class ClassSwatch(Flowable):
         self.canv.drawString(self.box + self.gap, 2, self.class_name.capitalize())
 
 
-def build_pdf_one_dataset(prot_images: Dict[int,list[io.BytesIO]], test_images:  Dict[str,list[io.BytesIO]],config_for_student,group,studentnr) -> list[Flowable]:
+def build_pdf_one_dataset(prot_images: Dict[int, list[io.BytesIO]], test_images: Dict[str, list[io.BytesIO]],
+                          config_for_student, group, studentnr) -> list[Flowable]:
     colour_names = ['pink', 'blue']
-    
+
     prototype_pdf = []
-    prototype_pdf.append(Paragraph(f"Configuration: {config_for_student}", BODY_TEXT_STYLE))
-    for i,label in enumerate(prot_images.keys()):
+    prototype_pdf.append(Paragraph(f"Configuration: <b>{config_for_student}</b>", BODY_TEXT_STYLE))
+    for i, label in enumerate(prot_images.keys()):
         one_class_images = prot_images[label]
         colour = 'pink' if label == 0 else 'blue'
-        #prototype_pdf.append(Paragraph(f"<font color='{colour}'>{colour}</font>", BODY_TEXT_STYLE))
+        # prototype_pdf.append(Paragraph(f"<font color='{colour}'>{colour}</font>", BODY_TEXT_STYLE))
         for img in one_class_images:
             buf_img = Image(img, width=3.5 * inch, height=1.3 * inch)
             prototype_pdf.append(buf_img)
 
-            data = [[ClassSwatch(c, getattr(colors, "white")) if c != colour else ClassSwatch(c, getattr(colors, colour)) for c in colour_names]]
-            table = Table(data, colWidths=[60]*len(colour_names))
+            data = [
+                [ClassSwatch(c, getattr(colors, "white")) if c != colour else ClassSwatch(c, getattr(colors, colour))
+                 for c in colour_names]]
+            table = Table(data, colWidths=[60] * len(colour_names))
             table.setStyle(TableStyle([("VALIGN", (0, 0), (-1, -1), "MIDDLE")]))
             prototype_pdf.append(table)
 
@@ -63,23 +65,25 @@ def build_pdf_one_dataset(prot_images: Dict[int,list[io.BytesIO]], test_images: 
 
         # images_pdf.append(PageBreak())
     test_pdf = []
-    test_pdf.append(Paragraph(f"Configuration: {config_for_student}", BODY_TEXT_STYLE))
+    test_pdf.append(Paragraph(f"Configuration: <b>{config_for_student}</b>", BODY_TEXT_STYLE))
 
-    #test_pdf.append(Paragraph("Which class do you think this is (pink or blue).", BODY_TEXT_STYLE))
-    test_pdf.append(Paragraph("<b>Test Samples</b> - Assign the colour you think corresponds to each of the following samples.", BODY_TEXT_STYLE))
+    # test_pdf.append(Paragraph("Which class do you think this is (pink or blue).", BODY_TEXT_STYLE))
+    test_pdf.append(
+        Paragraph("<b>Test Samples</b> - Assign the colour you think corresponds to each of the following samples.",
+                  BODY_TEXT_STYLE))
 
-    for i,img in enumerate(test_images):
+    for i, img in enumerate(test_images):
         if i == 5:
             test_pdf.append(FrameBreak())
+            test_pdf.append(Spacer(0, BODY_TEXT_STYLE.leading))
             test_pdf.append(Paragraph(f"Group:{group} Student:{studentnr}", BODY_TEXT_STYLE))
-            test_pdf.append(Spacer( 0, BODY_TEXT_STYLE.leading))
-
+            test_pdf.append(Spacer(0, BODY_TEXT_STYLE.leading))
 
         buf_img = Image(img, width=3.5 * inch, height=1.3 * inch)
         test_pdf.append(buf_img)
-        #test_pdf.append(Paragraph("Color: ", BODY_TEXT_STYLE))
-        data = [[f"{i+1})"] + [ClassSwatch(c, getattr(colors, "white")) for c in colour_names]]
-        table = Table(data, colWidths=[60]*(len(colour_names)+1))
+        # test_pdf.append(Paragraph("Color: ", BODY_TEXT_STYLE))
+        data = [[f"{i + 1})"] + [ClassSwatch(c, getattr(colors, "white")) for c in colour_names]]
+        table = Table(data, colWidths=[60] * (len(colour_names) + 1))
         table.setStyle(TableStyle([("VALIGN", (0, 0), (-1, -1), "MIDDLE")]))
         test_pdf.append(table)
 
@@ -89,7 +93,7 @@ def build_pdf_one_dataset(prot_images: Dict[int,list[io.BytesIO]], test_images: 
     return full_pdf
 
 
-def image_to_buf(ts: np.ndarray,y_lim:Tuple[float,float], class_num: str = -1,) -> io.BytesIO:
+def image_to_buf(ts: np.ndarray, y_lim: Tuple[float, float], class_num: str = -1, ) -> io.BytesIO:
     plt.figure(figsize=(6, 3))
     colour_dict = {0: 'pink', 1: 'blue'}
     colour = colour_dict.get(class_num, 'gray')
@@ -105,32 +109,37 @@ def image_to_buf(ts: np.ndarray,y_lim:Tuple[float,float], class_num: str = -1,) 
     plt.savefig(buf)
     plt.close()
     buf.seek(0)
-    from PIL import Image
     return buf
-def convert_to_images(proto_types:Dict[str,np.ndarray],test_instances:np.ndarray)->Tuple[Dict[str,List[io.BytesIO]],List[io.BytesIO]]:
+
+
+def convert_to_images(proto_types: Dict[str, np.ndarray], test_instances: np.ndarray) -> Tuple[
+    Dict[str, List[io.BytesIO]], List[io.BytesIO]]:
     all_values_proto = np.concatenate([lst.flatten() for lst in proto_types.values()])
     all_values_test = test_instances.flatten()
     all_values = np.concatenate([all_values_proto, all_values_test])
     min_val = np.min(all_values)
     max_val = np.max(all_values)
-    offset = ((max_val - min_val)/10)
-    y_lim = (min_val-offset, max_val+offset)
+    offset = ((max_val - min_val) / 10)
+    y_lim = (min_val - offset, max_val + offset)
     images_proto_types = {}
     for c in proto_types.keys():
-        images_proto_types[c] = [image_to_buf(ts=ts,y_lim=y_lim,class_num=c) for ts in proto_types[c]]
+        images_proto_types[c] = [image_to_buf(ts=ts, y_lim=y_lim, class_num=c) for ts in proto_types[c]]
 
-    images_test= [image_to_buf(ts=ts,y_lim=y_lim) for ts in test_instances]
+    images_test = [image_to_buf(ts=ts, y_lim=y_lim) for ts in test_instances]
     return images_proto_types, images_test
 
-def make_pdf_for_dataset_loyalty(dataset, loyalty_level,config_for_student, group, student_number):
-    full_train_instances_simplified,full_test_instances_simplified =get_train_and_test_instances(dataset, loyalty_level)
-    train_images, test_images = convert_to_images(full_train_instances_simplified,full_test_instances_simplified)
-    return build_pdf_one_dataset(train_images,test_images,config_for_student,group,student_number)
+
+def make_pdf_for_dataset_loyalty(dataset, loyalty_level, config_for_student, group, student_number):
+    full_train_instances_simplified, full_test_instances_simplified = get_train_and_test_instances(dataset,
+                                                                                                   loyalty_level)
+    train_images, test_images = convert_to_images(full_train_instances_simplified, full_test_instances_simplified)
+    return build_pdf_one_dataset(train_images, test_images, config_for_student, group, student_number)
+
 
 def make_pdf_for_all_datasets(dataset_loyalty_pairs, group, student_number):
     pdf = []
-    for i,(dataset, loyalty_level,config) in enumerate(dataset_loyalty_pairs):
-        pdf.extend(make_pdf_for_dataset_loyalty(dataset, loyalty_level,config, group,student_number))
+    for i, (dataset, loyalty_level, config) in enumerate(dataset_loyalty_pairs):
+        pdf.extend(make_pdf_for_dataset_loyalty(dataset, loyalty_level, config, group, student_number))
 
     doc = BaseDocTemplate(f"generate_user_survey/user_surveyes/{group}_{student_number}.pdf", pagesize=letter)
     ## -----------------  For two column layout -----------------
@@ -148,24 +157,23 @@ def make_pdf_for_all_datasets(dataset_loyalty_pairs, group, student_number):
     return pdf
 
 
-
 def make_pdf_for_current_student(group, student_number):
     config_for_student = get_config_of_group(group)
-    datasets_and_loyalty_for_student = [(*get_dataset_and_loyalty_from_config(config),config) for config in config_for_student]
+    datasets_and_loyalty_for_student = [(*get_dataset_and_loyalty_from_config(config), config) for config in
+                                        config_for_student]
     print(datasets_and_loyalty_for_student)
     make_pdf_for_all_datasets(datasets_and_loyalty_for_student, group, student_number)
-    print(group,student_number,datasets_and_loyalty_for_student)
+    print(group, student_number, datasets_and_loyalty_for_student)
 
 
 def build_all_surveys_full_survey():
     num_of_student_in_each_group = 4
-    for gi in range(1,5):
-        group =  "G{}".format(gi)
-        for si in range(1,1+num_of_student_in_each_group):
+    for gi in range(1, 5):
+        group = "G{}".format(gi)
+        for si in range(1, 1 + num_of_student_in_each_group):
             student_number = "S{}".format(si)
 
             make_pdf_for_current_student(group, student_number)
-
 
 
 if __name__ == "__main__":
